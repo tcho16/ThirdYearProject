7d3e9b5cdcaa0fe7f715d7c75a75ea51e94a66d5
tcho16 <tarik_chouhan@msn.com>
2017-09-26 17:02:35 +0100

    Downgraded dependency to v3.4.2
    And also added some dummy mongo commands

--- Spring Web Mongo/demo/demo/src/main/java/com/w1481217/demo/DemoApplication.java
+++ Spring Web Mongo/demo/demo/src/main/java/com/w1481217/demo/DemoApplication.java
@@ -1,12 +1,36 @@
 package com.w1481217.demo;
 
+import com.mongodb.*;
+import com.mongodb.client.MongoDatabase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+
+import java.util.Collection;
+import java.util.List;
 
 @SpringBootApplication(scanBasePackages = {"Controller"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		try {
+			// Creates a new instance of MongoDBClient and connect to localhost
+			// port 27017
+			MongoClient mongoClient = new MongoClient("localhost", 27017);
+
+            // Creating a database called Hello and creating a collection called HelloCollectionSS
+            MongoDatabase db = mongoClient.getDatabase("Hello");
+            db.createCollection("HelloCollectionSS");
+
+            //Displaying the current databases in Mongo
+            for(String name : mongoClient.listDatabaseNames()){
+                System.out.println(name);
+            }
+            
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
