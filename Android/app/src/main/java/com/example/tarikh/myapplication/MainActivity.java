0de5d03a9ccfc01648b74ffb7d37b8aa291b26ff
tcho16 <tarik_chouhan@msn.com>
2017-11-12 19:25:11 +0000

    Changes made to the android manifest and also the main activity.
    NOTE: App stops working when pressing the button. Currently debugging.

--- Android/app/src/main/java/com/example/tarikh/myapplication/MainActivity.java
+++ Android/app/src/main/java/com/example/tarikh/myapplication/MainActivity.java
@@ -1,17 +1,37 @@
 package com.example.tarikh.myapplication;
 
+import android.app.DownloadManager;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    RequestQueue requestQueue;
+    Button serviceButton;
+    TextView textViewService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        textViewService = findViewById(R.id.textViewServiceOutput) ;
+        requestQueue = Volley.newRequestQueue(this);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -24,6 +44,30 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void callTheService(View view) {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, "http://example.com",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        textViewService.setText(response.substring(0,133));
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+        Context context = getApplicationContext();
+        CharSequence text = "Failed to connect to the service!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+                    }
+                }
+        );
+
+        requestQueue.add(stringRequest);
+
     }
 
 }
