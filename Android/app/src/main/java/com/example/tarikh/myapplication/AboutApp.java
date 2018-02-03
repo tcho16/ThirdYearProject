f62619074070b407f2556afd9640f160574af6aa
tcho16 <tarik_chouhan@msn.com>
2018-02-03 16:10:49 +0000

    Edited the About Us page

--- Android/app/src/main/java/com/example/tarikh/myapplication/AboutApp.java
+++ Android/app/src/main/java/com/example/tarikh/myapplication/AboutApp.java
@@ -4,8 +4,10 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 
 public class AboutApp extends AppCompatActivity {
+    ImageView imageView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +16,9 @@ public class AboutApp extends AppCompatActivity {
         Toolbar myChildToolbar =
                 (Toolbar) findViewById(R.id.my_toolbarSecond);
         setSupportActionBar(myChildToolbar);
+        imageView = (ImageView) findViewById(R.id.imageView);
 
-        // Get a support ActionBar corresponding to this toolbar
+        imageView.setImageResource(R.drawable.screenshot);
         ActionBar ab = getSupportActionBar();
 
         // Enable the Up button
